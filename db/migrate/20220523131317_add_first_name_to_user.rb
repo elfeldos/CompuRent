db9652e9d0fbb124d8c389f2ecc9608bdb126157
Frederik Feldmeier <f.feldmeier98@googlemail.com>
2022-05-23 15:33:41 +0200

    initialized login/signup

---
+++ db/migrate/20220523131317_add_first_name_to_user.rb
@@ -0,0 +1,8 @@
+class AddFirstNameToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :username, :string
+    add_column :users, :status, :boolean
+  end
+end
