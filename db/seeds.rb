03d1471cf7ea1bf59a94523457ae8a15edcb8721
Frederik Feldmeier <f.feldmeier98@googlemail.com>
2022-05-24 11:56:45 +0200

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,4 +19,8 @@ Computer.create!(name: 'MacBookPro', category: 'Laptop', year: '2017', details: 
 Computer.create!(name: 'MacBookAir', category: 'Laptop', year: '2019', details: 'Heavily used', availiability: 'true', price:'72.99', user: johanna)
 Computer.create!(name: 'Razer Blade 15', category: 'Laptop', year: '2021', details: '11th Gen Intel® Core™ i7-11800H 8 Core (2.3GHz / 4.6GHz)', availiability: 'true', price:'99.99', user: johanna)
 Computer.create!(name: 'ASUS ROG Zephyrus G14', category: 'Laptop', year: '2020', details: 'Beeldscherm: 14 inch (2560 x 1440)', availiability: 'true', price:'35.99', user: freddy)
+Computer.create!(name: 'iMac', category: 'PC', year: '2019', details: 'Great shape!', availiability: 'true', price:'53.99', user: freddy)
+Computer.create!(name: 'Asus Zenbook', category: 'Laptop', year: '2016', details: 'Great graphics', availiability: 'true', price:'16.99', user: johanna)
+Computer.create!(name: 'Razer Blade 17', category: 'Laptop', year: '2022', details: '13th Gen Intel® Core™ i7-11800H 8 Core (2.3GHz / 4.6GHz)', availiability: 'true', price:'99.99', user: johanna)
+Computer.create!(name: 'HP Zephyrus G17', category: 'PC', year: '2020', details: 'Beeldscherm: 21 inch', availiability: 'true', price:'35.99', user: freddy)
 puts "done!"
