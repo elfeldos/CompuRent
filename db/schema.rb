f39a0197cce83d9d2acc65408bdc9c5b820e1dbe
Frederik Feldmeier <f.feldmeier98@googlemail.com>
2022-05-24 12:26:09 +0200

    added pics to seed - computers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_145557) do
+ActiveRecord::Schema.define(version: 2022_05_24_100523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2022_05_23_145557) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_computers_on_user_id"
   end
 
