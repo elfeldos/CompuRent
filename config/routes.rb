0ea6debb4cc7eece2979ef0d51545fb3f05f0f5b
Frederik Feldmeier <f.feldmeier98@googlemail.com>
2022-05-26 11:48:25 +0200

    accidently started without new branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/create'
   devise_for :users
   root to: 'pages#home'
   get 'components', to: 'pages#components'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # actual routes
+  resources :bookings, only: [:new, :create]
   resources :computers
   get '/', to: 'pages#home'
 end
