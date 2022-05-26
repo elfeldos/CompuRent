228a3bc61391ef0af125c0d9a9bb1a9370915cd0
Johanna Florange <florangejohanna@gmail.com>
2022-05-26 17:12:11 +0200

    how it works page added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'components', to: 'pages#components'
   get 'profile', to: 'pages#profile'
+  get 'how', to: 'pages#how', as: :howitworks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # actual routes
   resources :computers do
